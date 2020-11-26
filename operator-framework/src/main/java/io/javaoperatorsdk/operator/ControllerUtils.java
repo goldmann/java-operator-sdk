b9111b3718171fae76f20ba7ac8decba1d8df39f
Chris Laprun <metacosm@gmail.com>
2020-11-26 22:11:36 +0100

    fix: Controller doesn't have resource class field anymore

--- operator-framework/src/main/java/io/javaoperatorsdk/operator/ControllerUtils.java
+++ operator-framework/src/main/java/io/javaoperatorsdk/operator/ControllerUtils.java
@@ -1,11 +1,10 @@
 package io.javaoperatorsdk.operator;
 
+import java.util.Map;
+
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.client.CustomResourceDoneable;
-import io.javaoperatorsdk.operator.api.Controller;
 import io.javaoperatorsdk.operator.api.ResourceController;
-
-import java.util.Map;
 
 
 public class ControllerUtils {
@@ -14,32 +13,28 @@ public class ControllerUtils {
     
     public static final String CONTROLLERS_RESOURCE_PATH = "javaoperatorsdk/controllers";
     private static Map<Class<? extends ResourceController>, Class<? extends CustomResource>> controllerToCustomResourceMappings;
-        static {
-        controllerToCustomResourceMappings =
-                ControllerToCustomResourceMappingsProvider
-                        .provide(CONTROLLERS_RESOURCE_PATH);
+    
+    static {
+        controllerToCustomResourceMappings = ControllerToCustomResourceMappingsProvider.provide(CONTROLLERS_RESOURCE_PATH);
     }
     
     public static String getDefaultFinalizerName(String crdName) {
         return crdName + FINALIZER_NAME_SUFFIX;
     }
-
-    static <R extends CustomResource> Class<R> getCustomResourceClass(ResourceController<R> controller) {
-        final Class<? extends CustomResource> customResourceClass = controllerToCustomResourceMappings
-                .get(controller.getClass());
+    
+    public static <R extends CustomResource> Class<R> getCustomResourceClass(ResourceController<R> controller) {
+        final Class<? extends CustomResource> customResourceClass = controllerToCustomResourceMappings.get(controller.getClass());
         if (customResourceClass == null) {
             throw new IllegalArgumentException(
-                    String.format(
-                            "No custom resource has been found for controller %s",
-                            controller.getClass().getCanonicalName()
-                    )
+                String.format(
+                    "No custom resource has been found for controller %s",
+                    controller.getClass().getCanonicalName()
+                )
             );
         }
         return (Class<R>) customResourceClass;
     }
-
     
-
     public static <T extends CustomResource> Class<? extends CustomResourceDoneable<T>>
     getCustomResourceDoneableClass(ResourceController<T> controller) {
         try {
@@ -50,11 +45,7 @@ public class ControllerUtils {
             return null;
         }
     }
-
-    private static Controller getAnnotation(ResourceController<?> controller) {
-        return controller.getClass().getAnnotation(Controller.class);
-    }
-
+    
     public static boolean hasGivenFinalizer(CustomResource resource, String finalizer) {
         return resource.getMetadata().getFinalizers() != null && resource.getMetadata().getFinalizers().contains(finalizer);
     }
